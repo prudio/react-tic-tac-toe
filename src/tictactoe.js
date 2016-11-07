057ab6bb2676bb26e6c260b2467e1c27d088e8f5
Philip Rudio <prudio@uw.edu>
2016-11-06 21:49:36 -0800

    Add time-travel support to the game
    We also support changing history when going back to a previous state and
    selecting a different move.

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -44,12 +44,13 @@ class Game extends Component {
       history: [{
         squares: Array(9).fill(null)
       }],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -60,13 +61,21 @@ class Game extends Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ? false : true,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ?
